634992ed47d57fdf8a67de10635cc1d2c75fa446
kyosawa <osawa.kyohei@gmail.com>
2021-08-03 17:07:50 +0900

    DONE: https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/

--- rest_framework/tutorial/snippets/serializers.py
+++ rest_framework/tutorial/snippets/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets', 'owner']
